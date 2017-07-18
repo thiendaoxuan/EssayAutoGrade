25e56f8f7041bd46138bebd3b1c9bf24d0fa1ac3
thien <thiendaoxuan1@gmail.com>
2017-07-19 00:45:26 +0700

    init

---
+++ Preprocessing.py
@@ -0,0 +1,131 @@
+
+# -*- coding: ascii -*-
+import os, sys
+
+import pandas as pd
+
+import csv
+
+import string
+
+import nltk
+
+from time import time
+from nltk.corpus import stopwords as sw
+from nltk.corpus import wordnet as wn
+from nltk import wordpunct_tokenize
+from nltk import WordNetLemmatizer
+from nltk import sent_tokenize
+from nltk import pos_tag
+from functools32 import lru_cache
+
+
+class Preprocessor:
+    lemmatizer = WordNetLemmatizer()
+    stop_word = sw.words('english')
+
+    @staticmethod
+    def tokenize(doc):
+        list_lemma = []
+
+        try:
+            sent_tokenize(doc)
+        except Exception as e:
+            print ("cant tokenize : " + doc + str(e))
+            return
+
+        for sent in sent_tokenize(doc):
+
+            token_iter = iter(pos_tag(wordpunct_tokenize(sent)))
+
+            for token, tag in token_iter:
+
+                # ignore hidden name
+                if token[0] == '@':
+                    next(token_iter)
+
+                # clean up some token
+                token = token.lower()
+                token = token.strip()
+                token = token.strip('_')
+                token = token.strip('*')
+
+                try:
+                    token = token.encode('utf-8')
+                except Exception:
+                    continue
+
+                # If stopword, ignore token and continue
+                if token in set(Preprocessor.stop_word):
+                    continue
+
+                # If punctuation, ignore token and continue
+                if all(char in set(string.punctuation) for char in token):
+                    continue
+
+
+                # Lemmatize token
+                try:
+                    lemmatized_token = Preprocessor.__lemmatize(token, tag)
+                except Exception as e:
+                    continue
+
+                list_lemma.append(lemmatized_token)
+
+            list_lemma.append(".")
+
+        return " ".join(list_lemma)
+
+    @staticmethod
+    def __lemmatize(token, tag):
+        tag = {
+            'N': wn.NOUN,
+            'V': wn.VERB,
+            'R': wn.ADV,
+            'J': wn.ADJ
+        }.get(tag[0], wn.NOUN)
+
+        return Preprocessor.lemmatizer.lemmatize(token, tag)
+
+
+def select_based_id(data_input, id):
+    # new_x = data.filter(['essay_id','essay_set','essay', ''], axis=1)
+    return (data_input.loc[data_input["essay_set"] == id])
+
+
+# AutoGen file data
+if __name__ == "__main__":
+
+    print ('begin')
+
+    t = time();
+    print('Tokenizing : It works good but it goes slow sometimes but its a very good phone I love it')
+
+    print(Preprocessor.tokenize('It works good but it goes slow sometimes but its a very good phones I love it'))
+    print(time() - t)
+    t = time ()
+
+    with open('data/training_set_rel3.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        count = 0
+        t = time()
+
+        tuples = []
+
+        for row in spamreader:
+
+            count += 1
+            # Preprocessor.tokenize(row[4])
+            row[2] = Preprocessor.tokenize(row[2])
+
+            tuples.append(row);
+
+            if count%200 == 0:
+                print(tuples)
+                print ("current" + str(count))
+                print ("time to process last 200 : " + str(time() - t))
+                with open('data/tokenized_data.csv', 'a') as f:
+                    cw = csv.writer(f, lineterminator='\n')
+                    cw.writerows(r + [""] for r in tuples)
+                tuples = []
+                t = time()
---
+++ word2vec.py
@@ -0,0 +1,132 @@
+import numpy as np
+import re
+import itertools
+from collections import Counter
+
+import csv
+
+def clean_str(string):
+    """
+    Tokenization/string cleaning for datasets.
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
+
+
+def load_data_and_labels():
+    """
+    Loads polarity data from files, splits the data into words and generates labels.
+    Returns split sentences and labels.
+    """
+    # Load data from files
+    # positive_examples = list(open("./data/rt-polarity.pos", "r").readlines())
+    # positive_examples = [s.strip() for s in positive_examples[:1000]]
+    # negative_examples = list(open("./data/rt-polarity.neg", "r").readlines())
+    # negative_examples = [s.strip() for s in negative_examples[:1000]]
+    # # Split by words
+    # x_text = positive_examples + negative_examples
+    # x_text = [clean_str(sent) for sent in x_text]
+    # x_text = [s.split(" ") for s in x_text]
+    # # Generate labels
+    # positive_labels = [[0, 1] for _ in positive_examples]
+    # negative_labels = [[1, 0] for _ in negative_examples]
+    # y = np.concatenate([positive_labels, negative_labels], 0)
+
+    data = []
+    firstLine = True
+
+    filename = 'data/tokenized_data.csv'
+
+    label = [
+        [1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1],
+
+    ]
+
+    x_text = []
+    y = []
+
+    with open(filename, 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in spamreader:
+            if firstLine:
+                firstLine = False
+                continue
+            if (row[3] == "5" or row[3] == "4") and int(row[1]) < 6:
+                cur_row = clean_str(row[2])
+                row_arr = cur_row.split(" ")
+                x_text.append(row_arr)
+                y.append(label[int(row[1])-1])
+
+    return [x_text, np.asarray(y)]
+
+test = load_data_and_labels()
+print(test)
+
+
+def pad_sentences(sentences, padding_word="<PAD/>"):
+    """
+    Pads all sentences to the same length. The length is defined by the longest sentence.
+    Returns padded sentences.
+    """
+    sequence_length = max(len(x) for x in sentences)
+    padded_sentences = []
+    for i in range(len(sentences)):
+        sentence = sentences[i]
+        num_padding = sequence_length - len(sentence)
+        new_sentence = sentence + [padding_word] * num_padding
+        padded_sentences.append(new_sentence)
+    return padded_sentences
+
+
+def build_vocab(sentences):
+    """
+    Builds a vocabulary mapping from word to index based on the sentences.
+    Returns vocabulary mapping and inverse vocabulary mapping.
+    """
+    # Build vocabulary
+    word_counts = Counter(itertools.chain(*sentences))
+    # Mapping from index to word
+    vocabulary_inv = [x[0] for x in word_counts.most_common()]
+    vocabulary_inv = list(sorted(vocabulary_inv))
+    # Mapping from word to index
+    vocabulary = {x: i for i, x in enumerate(vocabulary_inv)}
+    return [vocabulary, vocabulary_inv]
+
+
+def build_input_data(sentences, labels, vocabulary):
+    """
+    Maps sentences and labels to vectors based on a vocabulary.
+    """
+    x = np.array([[vocabulary[word] for word in sentence] for sentence in sentences])
+    y = np.array(labels)
+    return [x, y]
+
+
+def load_data():
+    """
+    Loads and preprocessed data for the dataset.
+    Returns input vectors, labels, vocabulary, and inverse vocabulary.
+    """
+    # Load and preprocess data
+    sentences, labels = load_data_and_labels()
+    sentences_padded = pad_sentences(sentences)
+    vocabulary, vocabulary_inv = build_vocab(sentences_padded)
+    x, y = build_input_data(sentences_padded, labels, vocabulary)
+    return [x, y, vocabulary, vocabulary_inv]
